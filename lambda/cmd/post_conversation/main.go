9c62917015da53b25a25fc765b202bafc2332577
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2024-10-27 23:45:52 +0900

    fix main

--- lambda/cmd/post_conversation/main.go
+++ lambda/cmd/post_conversation/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/jumpei00/aws-bedrock-app/lambda/handler"
 )
 
-func handler() {
-	fmt.Println("Hello, World!")
-}
-
 func main() {
-	lambda.Start(handler)
+	handler, err := handler.NewPostConversationHandler("conversations")
+	if err != nil {
+		log.Fatalf("failed to create handler: %+v", err)
+	}
+	lambda.Start(handler.Handle)
 }
